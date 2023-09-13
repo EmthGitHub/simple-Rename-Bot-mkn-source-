60a6f92e8b621d4cf11236f5dc595f5d79885367
EmthGitHub <95978463+EmthGitHub@users.noreply.github.com>
2023-09-13 22:06:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "18368023"))
+API_HASH = environ.get("API_HASH", "10afacc8dc64d004422513027acf38d5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6473063321:AAFJqBrxh5LijaZrX6vECVY9VI9eLxKlnL4")
+ADMIN = int(environ.get("ADMIN", "696471140"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/6b428451ad67e7abbacca.jpg")
